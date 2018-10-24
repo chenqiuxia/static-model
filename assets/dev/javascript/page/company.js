5c6d9caffd7806275eb7ff6504fd7afe63a65ea4
823100926@qq.com <823100926@qq.com>
2018-10-24 22:25:11 +0800

    update

---
+++ assets/dev/javascript/page/company.js
@@ -0,0 +1,20 @@
+
+const initMap = () => {
+  var mapProp = {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+const init = () => {
+  initMap()
+}
+const bind = () => {
+
+}
+export default {
+  init: init(),
+  bind: bind()
+}
