bb69a2a756e7641775882b7ceaab046a310c163a
shinu0720 <shinu.hiratsuka@monstar-lab.com>
2018-10-25 02:24:40 +0800

    fix

--- assets/dev/javascript/consist/GLOBAL_CONFIG.js
+++ assets/dev/javascript/consist/GLOBAL_CONFIG.js
@@ -1,7 +1,7 @@
 import url from '../util/url'
 //ToDo: clear this global config
 //baseUrl
-export const baseUrl = $('meta[name="baseUrl"]').attr('content') || '/nippon/'
+
 
 // SP width
 export let isSP = document.body.clientWidth <= 782
