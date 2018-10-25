6c40f64db2f08a768fd32fb6a60e202cec03f376
823100926@qq.com <823100926@qq.com>
2018-10-25 09:20:53 +0800

    update

--- assets/dev/javascript/page/index.js
+++ assets/dev/javascript/page/index.js
@@ -1,5 +1,19 @@
-const init = () => {
+const initBanner = () => {
+  $(document).ready(function(){
+    $('.x-banner-slick').slick({
+      infinite: true,
+      pauseOnFocus: false,
+      speed: 2000,
+      fade: true,
+      cssEase: 'linear',
+      autoplay: true,
+      arrows: false
+    });
+  });
+}
 
+const init = () => {
+  initBanner()
 }
 const bind = () => {
 
