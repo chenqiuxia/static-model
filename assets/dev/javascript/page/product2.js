38c4145c2e7c26e842611ee49ef684ba7cd48f1c
mrliang <619727242@qq.com>
2018-10-25 09:55:42 +0800

    6

--- assets/dev/javascript/page/product2.js
+++ assets/dev/javascript/page/product2.js
@@ -15,8 +15,30 @@ const bottomHover = ()=>{
     })
 }
 
+const _animation = () => {
+    var $scroll = $('.heatset-product '),
+          window_scroll_top;
+    var bind = function () {
+        $(window).scroll(function () {
+            animation();
+        });
+    };
+    var animation = function () {
+        window_scroll_top = $(window).scrollTop();
+        
+        $scroll.each(function () {
+            if (window_scroll_top + window.innerHeight >= ($(this).offset().top) +200){
+                $(this).addClass('is_scoped');
+            }
+        });
+    };
+    bind();
+    animation();
+}
+
   const init = () => {
-    // bottomHover()
+     bottomHover();
+     _animation();
   }
   const bind = () => {
 
