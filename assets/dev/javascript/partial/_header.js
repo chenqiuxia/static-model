bbbb81df163f58314e0a4956e316f36f93a651ec
yiCoy <823100926@qq.com>
2018-11-01 22:11:14 +0800

    scroll

--- assets/dev/javascript/partial/_header.js
+++ assets/dev/javascript/partial/_header.js
@@ -1,5 +1,21 @@
 import {isSP, isHSFn} from "../consist/GLOBAL_CONFIG";
 
+const slidebarFixed = (function(bodyCls) {
+  var scrollTop;
+  return {
+    afterOpen: function() {
+      scrollTop = document.scrollingElement.scrollTop
+      document.body.classList.add(bodyCls)
+      document.body.style.top = -scrollTop + 'px'
+    },
+    beforeClose: function() {
+      document.body.classList.remove(bodyCls)
+      // scrollTop lost after set position:fixed, restore it back.
+      document.scrollingElement.scrollTop = scrollTop
+    }
+  };
+})('no-scroll')
+
 const onScroll = () => {
   $(window).scroll(function () {
     if ($(this).scrollTop() > 0) {
@@ -14,9 +30,14 @@ const onScroll = () => {
 const menuFn = () => {
   $('.x-menu').click(function (e) {
     e.stopPropagation()
+    if(!$('body').hasClass('no-scroll')) {
+      slidebarFixed.afterOpen()
+    } else {
+      slidebarFixed.beforeClose()
+    }
     $(this).toggleClass('menu-close')
     $('.x-header').toggleClass('header-slide')
-    $('body').toggleClass('no-scroll')
+    // $('body').toggleClass('no-scroll')
   })
 }
 
